175d7de4e9b482e29385b6ba5b3fb4fd7accaa40
superz97 <b.zangar15@gmail.com>
2025-10-10 19:49:40 +0500

    Add auth dtos

---
+++ src/main/java/com/github/superz97/tracker/dto/request/RefreshTokenRequest.java
@@ -0,0 +1,18 @@
+package com.github.superz97.tracker.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenRequest {
+
+    @NotBlank(message = "Refresh token is required")
+    private String refreshToken;
+
+}
---
+++ src/main/java/com/github/superz97/tracker/dto/response/JwtResponse.java
@@ -0,0 +1,19 @@
+package com.github.superz97.tracker.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType = "Bearer";
+    private Long expiresIn;
+
+}
